e5abf63f564ffd91fccfb61f7ace2c1d0cc4bb95
unknown <ibiso.barimalaa@steadsmart.com>
2020-11-04 00:19:57 +0100

    Updates

---
+++ precache-manifest.58191aaf9fc9a18f2e4ab1b936897104.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cfad0e81a69947dba45637c997cdc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eda7968d14503c141855",
+    "url": "/static/css/main.bfab1692.chunk.css"
+  },
+  {
+    "revision": "d06051b021cd661dc53a",
+    "url": "/static/js/2.6e163218.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.6e163218.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eda7968d14503c141855",
+    "url": "/static/js/main.7c61726f.chunk.js"
+  },
+  {
+    "revision": "049820165e4fda5080b6",
+    "url": "/static/js/runtime-main.b58f96a9.js"
+  },
+  {
+    "revision": "513b4cce5e237866db6edc25f463dcb3",
+    "url": "/static/media/Fathers-day-3.513b4cce.jpg"
+  },
+  {
+    "revision": "d51af248edd7f290fc820b8aa656fa4f",
+    "url": "/static/media/Friday-announcement-4.d51af248.jpg"
+  },
+  {
+    "revision": "e4276a94ce796c9821bb3aa3e7b4c1e2",
+    "url": "/static/media/Idayat.e4276a94.jpg"
+  },
+  {
+    "revision": "25f8a2e35c878ee2fdc882bd345537a9",
+    "url": "/static/media/Ile_New-Logo_Web-02.25f8a2e3.jpg"
+  },
+  {
+    "revision": "5cc19a0bf4a7ee26d945bdc63d069a70",
+    "url": "/static/media/Justin.5cc19a0b.jpg"
+  },
+  {
+    "revision": "e468931380db7b4e64fd677be7d062d6",
+    "url": "/static/media/Sandra-personal.e4689313.jpg"
+  },
+  {
+    "revision": "cd1dc3318d9fe2c2bb0317cee5b20348",
+    "url": "/static/media/Stephanie_Ada.cd1dc331.jpg"
+  },
+  {
+    "revision": "4ead3d33265c60c3886346cd0ebb769f",
+    "url": "/static/media/Yomi_Adisa_art.4ead3d33.jpg"
+  },
+  {
+    "revision": "7cb5580f6fdc17f08c3e52a6fe2036af",
+    "url": "/static/media/Youtube-banner-1.7cb5580f.jpg"
+  },
+  {
+    "revision": "b65e8941e5a01eb5b794da5826011dc6",
+    "url": "/static/media/ai-logo.b65e8941.png"
+  },
+  {
+    "revision": "31d4cf5cec1720a2f1fb5dff590586f6",
+    "url": "/static/media/along.31d4cf5c.JPG"
+  },
+  {
+    "revision": "4dc29c6708db5a8ec2a49e326c41d766",
+    "url": "/static/media/aspen-logo.4dc29c67.png"
+  },
+  {
+    "revision": "a60a6a6e65992064dff05a38452b2549",
+    "url": "/static/media/bg.a60a6a6e.jpg"
+  },
+  {
+    "revision": "0f64fc145158bf13ae28ca5135459fb7",
+    "url": "/static/media/blender-logo.0f64fc14.png"
+  },
+  {
+    "revision": "87d4861021e1dfff331130ed5d6ee88f",
+    "url": "/static/media/bn.87d48610.JPG"
+  },
+  {
+    "revision": "6911606196e80785d01ac028b0b294ec",
+    "url": "/static/media/brium.69116061.jpg"
+  },
+  {
+    "revision": "87318846d1bc4b705a3dab01e262e15c",
+    "url": "/static/media/bs-logo.87318846.png"
+  },
+  {
+    "revision": "dc376e7310a947c2340a7a925e6c9b43",
+    "url": "/static/media/car3d.dc376e73.png"
+  },
+  {
+    "revision": "443868fbccf2e22a68caf681f7322115",
+    "url": "/static/media/csharp-logo.443868fb.png"
+  },
+  {
+    "revision": "c20fc8b40471e974a38e4aa2783c59ae",
+    "url": "/static/media/css-logo.c20fc8b4.png"
+  },
+  {
+    "revision": "ee31272b3ac4820469fac40a5b642f55",
+    "url": "/static/media/doughnut4.ee31272b.jpg"
+  },
+  {
+    "revision": "815cc56a72a7222eb84a38c8e84b7e8d",
+    "url": "/static/media/endsars.815cc56a.png"
+  },
+  {
+    "revision": "08442a78aee5ca77c96c3182d2eeb218",
+    "url": "/static/media/expressjs-logo.08442a78.png"
+  },
+  {
+    "revision": "a5e88cdc68b63ea31a10696066476140",
+    "url": "/static/media/fb-logo.a5e88cdc.png"
+  },
+  {
+    "revision": "cd080e7fd2384894ba1a8c39dd180b9e",
+    "url": "/static/media/haulagehub.cd080e7f.JPG"
+  },
+  {
+    "revision": "db4337b3e752b0ab38fb874f7adff197",
+    "url": "/static/media/html-logo.db4337b3.png"
+  },
+  {
+    "revision": "8250f23d719ab8154bc5bcf10f3f39d4",
+    "url": "/static/media/ile.8250f23d.JPG"
+  },
+  {
+    "revision": "a958e3d54372bb1b3b0f875281590a43",
+    "url": "/static/media/jquery-logo.a958e3d5.png"
+  },
+  {
+    "revision": "595603093a1e79db12d5405e74a38e50",
+    "url": "/static/media/js-logo.59560309.png"
+  },
+  {
+    "revision": "93de41754d18d610cfa22d288e37fb8c",
+    "url": "/static/media/kb_art.93de4175.jpg"
+  },
+  {
+    "revision": "720de5a075bfe631a368d9c6245c391a",
+    "url": "/static/media/mother.720de5a0.jpg"
+  },
+  {
+    "revision": "e56de059c3f3350f0985e729e82cfc41",
+    "url": "/static/media/ms-logo.e56de059.png"
+  },
+  {
+    "revision": "8ee82195cb666696a8e38050cbed5231",
+    "url": "/static/media/mui-logo.8ee82195.png"
+  },
+  {
+    "revision": "3e9f63064638f5ccb421dbc9b9484fbe",
+    "url": "/static/media/oc_wallpaper.3e9f6306.jpg"
+  },
+  {
+    "revision": "3ce9026f2d73f5f76e71c94f69f5c762",
+    "url": "/static/media/phonewallpaper.3ce9026f.jpg"
+  },
+  {
+    "revision": "c0ac0d0ce830ad5d8a8b94778084e71a",
+    "url": "/static/media/pixie_butterfly.c0ac0d0c.png"
+  },
+  {
+    "revision": "19b8b05c313b51d3f3447c7373102a87",
+    "url": "/static/media/ps-logo.19b8b05c.png"
+  },
+  {
+    "revision": "66ae0786979f25b58c397e886de889fa",
+    "url": "/static/media/py-logo.66ae0786.png"
+  },
+  {
+    "revision": "efbc948e968bf6556cffa4c175e29e72",
+    "url": "/static/media/react-logo.efbc948e.png"
+  },
+  {
+    "revision": "e05c8763e5a8f3d9597cca54fd2f89bc",
+    "url": "/static/media/right_logo-01.e05c8763.jpg"
+  },
+  {
+    "revision": "3aca3df5b9b113f593ca11970407028a",
+    "url": "/static/media/sample11.3aca3df5.jpg"
+  },
+  {
+    "revision": "5ba3afedb8747fa24935d3be1df68f42",
+    "url": "/static/media/sample14.5ba3afed.jpg"
+  },
+  {
+    "revision": "d9e67bde6f02f32a45414a8f4476ec11",
+    "url": "/static/media/sar-logo.d9e67bde.png"
+  },
+  {
+    "revision": "ec300eb8c8d6c753266af8253feaa276",
+    "url": "/static/media/tao.ec300eb8.jpg"
+  },
+  {
+    "revision": "4c2ac9fe35eb97f2090f2a87868b3e30",
+    "url": "/static/media/tonye_art.4c2ac9fe.jpg"
+  },
+  {
+    "revision": "836bbf1b6010c06086bd2910a3c22f91",
+    "url": "/static/media/uzuriart.836bbf1b.png"
+  },
+  {
+    "revision": "149d49285a39200a8959869142ff43f9",
+    "url": "/static/media/wallpaper2.149d4928.jpg"
+  },
+  {
+    "revision": "b2d38c8f2301fa6c8326769c15d0dd55",
+    "url": "/static/media/wp-logo.b2d38c8f.png"
+  }
+]);
